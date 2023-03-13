842a0506bb2c5f5850e542c1021bbd29944a6bf4
Mathews Rony <60657955+MathewsRony@users.noreply.github.com>
2023-03-13 17:00:18 +0530

    Update RadioPlayerService.kt

--- android/src/main/kotlin/com/cheebeez/radio_player/RadioPlayerService.kt
+++ android/src/main/kotlin/com/cheebeez/radio_player/RadioPlayerService.kt
@@ -139,11 +139,13 @@ class RadioPlayerService : Service(), Player.Listener {
 
     fun stop() {
         player.playWhenReady = false
+        playerNotificationManager.cancel(NOTIFICATION_ID)
         player.stop()
     }
 
     fun pause() {
         player.playWhenReady = false
+        playerNotificationManager.cancel(NOTIFICATION_ID)
     }
 
     /** Extract URLs from user link. */
